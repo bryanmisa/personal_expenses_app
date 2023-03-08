70be10cb6c5bcabd59fa25d79d0dc08fc93ced35
Bryan Robertson Misa <bryan.misa@gmail.com>
2023-03-08 20:11:02 +0300

    updated the widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transactions.dart';
 
 // class imports
+import './widgets/transaction_list.dart';
+import './models/transaction.dart';
+import './widgets/new_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,7 +17,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
 /*
 1. Card() always assumes the size of the child widget          
 2. Text widgets only take the only space needs, like the length,
@@ -24,20 +31,71 @@ of the texts.
 4. width : double.infinity , parameter will occupy the whole row space of the
 screen
 */
+  // final titleController = TextEditingController();
+  // final amountController = TextEditingController();
 
-// PROPERTIES
-  // For Example purposes
-  // late String titleInput;
-  // late String amountInput;
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    // Transaction(
+    //   id: 't3',
+    //   title: 'Car Gas',
+    //   amount: 10.24,
+    //   date: DateTime.now(),
+    // ),
+  ];
 
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+// Class Methods
+  void _startAddNewTransaction(BuildContext ctx) {
+    // builder is a function that needs to be return a widget
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          // when the user tap on inside the modal bottom sheet it will close
+          // but will close when tapped outside the modal sheet
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Expense Tracker'),
+        // adding buttons on the appbar using the iconButton
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: Icon(Icons.add),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -52,9 +110,16 @@ screen
                 elevation: 5,
               ), //Card of the Chart Container
             ), // Container Chart
-            UserTransactions()
+            TransactionList(_userTransactions),
           ], // children widget of Column
         ),
+      ),
+      // Adding a floating action button and positioning the location of the
+      // button
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
